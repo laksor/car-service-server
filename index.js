273dd96516aded5193f5e9e3e4e75925f22f8ae9
laskor <al.moksud@gmail.com>
2022-06-10 03:11:18 -0400

    service section backend added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,8 +27,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const userCollection = client.db("car-service").collection("users");
-   console.log('db connected');
+    const serviceCollection = client.db('car').collection('services');
+   
+    app.get('/service', async (req,res) =>{
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services);
+   })
+   
+   app.get('/service/:id', async (req,res) =>{
+     const id = req.params.id;
+     const query = {_id: ObjectId(id)};
+     const service = await serviceCollection.findOne(query);
+     res.send(service);
+   })
+
 } finally {
 
   }
@@ -35,7 +50,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("running car server");
+  res.send("car service server");
 });
 
 app.listen(port, () => {
