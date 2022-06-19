26be1b4fa904b06bf2256bea1e568fb1d08cf284
laskor <al.moksud@gmail.com>
2022-06-19 01:10:47 -0400

    delete api added

--- index.js
+++ index.js
@@ -49,7 +49,15 @@ async function run() {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
+   })
 
+   //Delete
+
+   app.delete('/service/:id', async (req,res) =>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await serviceCollection.deleteOne(query);
+    res.send(result);
    })
 
 } finally {
