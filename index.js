7032e6ada6c294b833a34d5f38b9c8afc3a2e0f6
laskor <al.moksud@gmail.com>
2022-07-21 22:51:30 -0400

    token

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
 
    //order collection
 
+   app.get('/order', async(req,res) =>{
+    const email = req.query.email;
+    const query = {email: email};
+    const cursor = orderCollection.find(query);
+    const orders = await cursor.toArray();
+    res.send(orders); 
+   })
+
    app.post('/order', async(req,res) => {
     const order = req.body;
     const result = await orderCollection.insertOne(order);
