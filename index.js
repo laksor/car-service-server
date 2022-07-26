9991b3ae9610da94ddb3c18fbce73bb1c031c4ac
laskor <al.moksud@gmail.com>
2022-07-25 20:22:35 -0400

    accesstoken

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ function verifyJWT(req, res, next){
         return res.status(403).send({message: 'Forbidden access'});
       }
       req.decoded = decoded;
+      next();
     })
-    next();
+    
 }
 
 // connecting mongodb and crud
